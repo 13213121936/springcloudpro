306b555d926175a77e4d912e3a32b11b952578d6
zx1712050774 <1712050774@qq.com>
2019-04-17 10:07:48 +0800

    测试

---
+++ springcloud-feign/src/main/java/com/jk/Controller/PageController.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C),
+ * FileName: PageController
+ * Author:   zx
+ * Date:     2019/4/17 9:40
+ * History:
+ * zx         <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * @author 17120
+ * @create 2019/4/17
+ * @since 1.0.0
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("toAddCar")
+    public String toAddCar(){
+        return "addCar";
+    }
+}
